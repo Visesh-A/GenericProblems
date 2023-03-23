c929772dd2ccc8f422cb634101bd4b7900bc1404
“Visesh-A” <viseshajl@gmail.com>
2023-03-23 19:42:01 +0530

    [visesh]Add: Refractor2

--- GenericsProblems/Program.cs
+++ GenericsProblems/Program.cs
@@ -10,11 +10,11 @@ namespace GenericsProblems
         static void Main(string[] args)
         {
             MaximumGeneric<int> maximum1 = new MaximumGeneric<int>(12, 82, 43);
-            maximum1.FindMax();
-            MaximumGeneric<float> maximum2 = new MaximumGeneric<float>(12, 8, 43);
-            maximum2.FindMax();
+            maximum1.FindMaxNum();
+            MaximumGeneric<float> maximum2 = new MaximumGeneric<float>(11, 8, 42);
+            maximum2.FindMaxNum();
             MaximumGeneric<string> maximum3 = new MaximumGeneric<string>("Apple", "Peach", "Banana");
-            maximum3.FindMax();
+            maximum3.FindMaxNum();
         }
     }
 
